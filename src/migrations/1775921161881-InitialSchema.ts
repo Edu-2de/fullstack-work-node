1526e8f3182326a2d51e385c6482dcc8d995c7a5
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-04-11 12:28:51 -0300

    Atualiza scripts de migração no package.json e adiciona nova migração para criação das tabelas de usuários, eventos e categorias

--- src/migrations/1775921161881-InitialSchema.ts
+++ src/migrations/1775921161881-InitialSchema.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUserTable1775864279029 implements MigrationInterface {
-    name = 'CreateUserTable1775864279029'
+export class InitialSchema1775921161881 implements MigrationInterface {
+    name = 'InitialSchema1775921161881'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "UQ_8b0be371d28245da6e4f4b61878" UNIQUE ("name"), CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`);
