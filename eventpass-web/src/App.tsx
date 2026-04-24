0a525592411c029e3b1f0648d3524d9ab449c457
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-04-24 19:04:30 -0300

    feat: update package.json dependencies and add tailwind-merge
    feat: enhance App component with Text component for better typography
    
    refactor: improve Text component to support color variants and clean up props
    
    style: adjust CSS variables for gray colors and set body background color

--- eventpass-web/src/App.tsx
+++ eventpass-web/src/App.tsx
@@ -1,7 +1,19 @@
+import Text from "./components/text";
+
 function App() {
     return (
         <>
-            <div className="hero"></div>
+            <div className="w-full flex flex-col items-center gap-2 bg-gray-100 mt-5">
+                <Text as="h2" variant="display-xl" weight="bold">
+                    Display Xl
+                </Text>
+                <Text as="h1" variant="title-hg" weight="bold">
+                    Title Hg
+                </Text>
+                <Text as="p" variant="input-text" weight="bold">
+                    Input
+                </Text>
+            </div>
         </>
     );
 }
--- eventpass-web/src/components/text.tsx
+++ eventpass-web/src/components/text.tsx
@@ -2,54 +2,62 @@ import React from "react";
 import { tv, type VariantProps } from "tailwind-variants";
 
 const textVariants = tv({
-    base: "text-white",
     variants: {
         variant: {
-            "display-xl": "font-display text-2xl",
-            "display-lg": "font-display text-xl",
-            "display-md": "font-display text-base",
+            "display-xl": "font-display text-2xl leading-normal",
+            "display-lg": "font-display text-xl leading-normal",
+            "display-md": "font-display text-base leading-normal",
 
-            "title-hg": "font-title text-[2rem] font-bold",
-            "title-xl": "font-title text-2xl font-bold",
-            "title-lg": "font-title text-xl font-bold",
-            "title-xs": "font-title text-xs font-normal",
+            "title-hg": "font-title text-[2rem] leading-normal",
+            "title-xl": "font-title text-2xl leading-normal",
+            "title-lg": "font-title text-xl leading-normal",
+            "title-xs": "font-title text-xs leading-normal",
 
-            "input-text": "font-body text-[1rem] leading-6",
-            "text-md": "font-body text-[1rem] leading-[160%]",
-            "text-sm": "font-body text-[0.875rem] leading-[160%]",
-            "text-xs": "font-body text-[0.75rem] leading-[160%]",
+            "input-text": "font-body text-base leading-6",
+            "text-md": "font-body text-base leading-[160%]",
+            "text-sm": "font-body text-sm leading-[160%]",
+            "text-xs": "font-body text-xs leading-[160%]",
         },
         weight: {
             regular: "font-normal",
             bold: "font-bold",
         },
+        color: {
+            main: "text-gray-700",
+            secondary: "text-gray-600",
+            ghost: "text-gray-500",
+            selected: "text-purple-light",
+            button: "text-white",
+        },
     },
     defaultVariants: {
         variant: "text-md",
         weight: "regular",
+        color: "secondary",
     },
 });
 
 interface TextProps extends VariantProps<typeof textVariants> {
-    as?: keyof React.JSX.IntrinsicElements;
+    as?: React.ElementType;
     className?: string;
     children?: React.ReactNode;
 }
 
 export default function Text({
-    as = "span",
+    as: Component = "span",
     className,
     variant,
+    color,
     weight,
     children,
     ...props
 }: TextProps) {
-    return React.createElement(
-        as,
-        {
-            className: textVariants({ variant, weight, className }),
-            ...props,
-        },
-        children,
+    return (
+        <Component
+            className={textVariants({ variant, weight, color, className })}
+            {...props}
+        >
+            {children}
+        </Component>
     );
 }
